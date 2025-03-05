402455a32034a6cd5e1805bdbfebbe7a4e7620d9
J. Charles N. M <jcnm@sylorion.com>
2025-03-05 21:09:16 +0100

    Clean up to be close to bef-utils

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+// jest.config.js
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
 };
+
+export default config;
